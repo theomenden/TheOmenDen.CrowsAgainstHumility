8a3d4955744f25eb06c2f9907709b138a608db0e
Andrew Beers <andrew.d.beers@gmail.com>
2022-09-11 17:29:07 -0400

    Add rule sets to the GameTypes and Display them. Add legal disclaimers

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-[assembly:System.Reflection.AssemblyFileVersion("1.09.11.0541")]
+[assembly:System.Reflection.AssemblyFileVersion("1.09.11.1732")]
 [assembly:System.Reflection.AssemblyCompany("The Omen Den L.L.C.")]
--- TheOmenDen.CrowsAgainstHumility.Core/Rules/GameTypes.cs
+++ TheOmenDen.CrowsAgainstHumility.Core/Rules/GameTypes.cs
@@ -17,4 +17,17 @@ public sealed record GameTypes : EnumerationBase<GameTypes>
     public static readonly GameTypes SurvivalOfTheFittest = new("Survival of the Fittest",6);
     public static readonly GameTypes SeriousBusiness = new("Serious Business", 7);
     public static readonly GameTypes NeverHaveIEver = new("Never Have I Ever", 8);
+    public static readonly GameTypes WheatonsLaw = new("Wheaton's Law", 9);
+    public static readonly GameTypes ChubbyBunny = new("Chubby Bunny", 10);
+    public static readonly GameTypes FreakyFriday = new("Freaky Friday", 11);
+    public static readonly GameTypes HailToTheCheif = new("Hail to the Chief", 12);
+    public static readonly GameTypes HardMode = new("Hard Mode", 13);
+    public static readonly GameTypes Meritocracy = new("Meritocracy", 14);
+    public static readonly GameTypes RaceToTheMoon = new("Race To The Moon", 15);
+    public static readonly GameTypes RussianRoulette = new("Russian Roulette", 16);
+    public static readonly GameTypes SmokeOpiumAndPlay = new("Smoke Opium and Play Cards Against Humanity", 17);
+    public static readonly GameTypes TieBreaker = new("Tie Breaker", 18);
+    public static readonly GameTypes WaitForGodot = new("Wait for Godot", 19);
+    public static readonly GameTypes SmoothOperator = new("Smooth Operator", 20);
+    public static readonly GameTypes DontPlay = new("Don't Play Cards Against Humanity", 21);
 }
