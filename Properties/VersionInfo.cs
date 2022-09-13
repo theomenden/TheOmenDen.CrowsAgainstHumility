29d65011bff1220ca4691472e7078b76053b36c5
Andrew Beers <andrew.d.beers@gmail.com>
2022-09-13 10:53:56 -0400

    Update License Expression, remove Apache-2

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-[assembly:System.Reflection.AssemblyFileVersion("1.09.12.2351")]
+[assembly:System.Reflection.AssemblyFileVersion("1.09.13.1051")]
 [assembly:System.Reflection.AssemblyCompany("The Omen Den L.L.C.")]
