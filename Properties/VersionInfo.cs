3428b4d2208b0102e31a8cfa6f66877eec3ce052
Andrew Beers <andrew.d.beers@gmail.com>
2022-09-15 11:04:00 -0400

    Add cookie stuff

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,2 +1,3 @@
-[assembly:System.Reflection.AssemblyFileVersion("1.09.14.1054")]
+[assembly:System.Reflection.AssemblyFileVersion("1.09.15.1143")]
 [assembly:System.Reflection.AssemblyCompany("The Omen Den L.L.C.")]
+[assembly:System.Reflection.AssemblyTitle("Crows Against Humility")]
