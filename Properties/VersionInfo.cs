4c02fa0f9f9a5e4cf902be89af591dc42d391df8
Andrew Beers <andrew.d.beers@gmail.com>
2022-09-15 13:02:49 -0400

    Add Privacy Police, and Terms of Service pages. Add script loader

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,3 +1,3 @@
-[assembly:System.Reflection.AssemblyFileVersion("1.09.15.1143")]
+[assembly:System.Reflection.AssemblyFileVersion("1.09.15.1325")]
 [assembly:System.Reflection.AssemblyCompany("The Omen Den L.L.C.")]
 [assembly:System.Reflection.AssemblyTitle("Crows Against Humility")]
---
+++ wwwroot/js/scriptLoader.js
@@ -0,0 +1,41 @@
+﻿function loadJs(sourceUrl) {
+	if (sourceUrl.Length == 0) {
+		console.error("Invalid source URL");
+		return;
+	}
+
+	let tag = document.createElement('script');
+	tag.src = sourceUrl;
+	tag.type = "text/javascript";
+
+	tag.onload = function () {
+		console.log("Script loaded successfully");
+	}
+
+	tag.onerror = function () {
+		console.error("Failed to load script");
+	}
+
+	document.body.appendChild(tag);
+}
+
+function loadJsById(scriptId, sourceUrl) {
+	if (sourceUrl.Length == 0) {
+		console.error("Invalid source URL")
+	}
+
+	let tag = document.createElement('script');
+	tag.src = sourceUrl;
+	tag.type = "text/javascript";
+	tag.id = scriptId;
+
+	tag.onload = function () {
+		console.log("Script loaded successfully");
+	}
+
+	tag.onerror = function () {
+		console.error("Failed to load script");
+	}
+
+	document.body.appendChild(tag);
+}
