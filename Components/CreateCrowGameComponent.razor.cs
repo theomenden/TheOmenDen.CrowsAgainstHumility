e81f934fcaad65f7966019e02f9f9cc72f163630
Andrew Diego Beers <andrew.d.beers@gmail.com>
2023-03-25 02:17:40 -0400

    Add more crowgame stuff for twitch and such

--- Components/CreateCrowGameComponent.razor.cs
+++ Components/CreateCrowGameComponent.razor.cs
@@ -3,12 +3,18 @@ using Blazorise.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TheOmenDen.CrowsAgainstHumility.Core.Constants;
+using TheOmenDen.CrowsAgainstHumility.Core.Enumerations;
 using TheOmenDen.CrowsAgainstHumility.Core.Interfaces.Services;
+using TheOmenDen.CrowsAgainstHumility.Core.Models;
 using TheOmenDen.CrowsAgainstHumility.Core.Models.CrowGames;
 using TheOmenDen.CrowsAgainstHumility.Data.Contexts;
 using TheOmenDen.CrowsAgainstHumility.Services;
 using TheOmenDen.CrowsAgainstHumility.Services.Authentication;
+using TheOmenDen.Shared.Enumerations.Structs;
 using TheOmenDen.Shared.Extensions;
+using TwitchLib.Api.Helix;
 using TwitchLib.Api.Helix.Models.Users.GetUsers;
 #nullable disable
 namespace TheOmenDen.CrowsAgainstHumility.Components;
@@ -16,32 +22,152 @@ namespace TheOmenDen.CrowsAgainstHumility.Components;
 public partial class CreateCrowGameComponent : ComponentBase, IDisposable, IAsyncDisposable
 {
     private const string ModalTitle = "Player Does Not Exist";
-
+    #region Cascading Parameters
     [CascadingParameter] public Task<AuthenticationState> AuthenticationStateTask { get; set; }
+    #endregion
+    #region Injected Services
+    [Inject] private IMessageService MessageService { get; init; }
 
     [Inject] private IModalService ModalService { get; init; }
 
     [Inject] private IPackViewService PackViewService { get; init; }
 
+    [Inject] private IPlayerVerificationService PlayerVerificationService { get; init; }
+
+    [Inject] private UserManager<ApplicationUser> UserManager { get; init; }
+    #endregion
+    #region Private Pack Models
     private IEnumerable<Pack> _packs = Enumerable.Empty<Pack>();
-
-    private readonly CreateCrowGameInputModel _createModal = new ();
-
+    private List<Guid> _selectedPackIds = new(10);
+    private List<string> _selectedPackNames = new(10);
+    #endregion
+    #region Private User Models
+    private User _selectedPlayer;
+    private string _playerToAdd = String.Empty;
+    private readonly List<User> _players = new(10);
+    #endregion
+    #region Crow Game Input Models
+    private readonly CreateCrowGameInputModel _createModal = new();
+    #endregion
     protected override async Task OnInitializedAsync()
     {
         await base.OnInitializedAsync();
         _createModal.RoomCode = GameCodeGenerator.GenerateGameCodeFromComponent(nameof(CreateCrowGameComponent));
-        _packs = await PackViewService.GetPacksForGameCreationAsync();
+        _packs = (await PackViewService.GetPacksForGameCreationAsync()).ToArray();
     }
 
-    private Task OnResetClickedAsync()
+    #region Pack Manipulation Methods
+    private int GetEstimatedWhiteCardCount()
+    => _selectedPackIds
+            .Sum(selectedPackId =>
+                _packs
+                    .FirstOrDefault(f => f.Id == selectedPackId)
+                    ?.WhiteCardsInPack ?? 0);
+
+    private int GetEstimatedBlackCardCount()
+        => _selectedPackIds
+            .Sum(selectedPackId =>
+                _packs
+                    .FirstOrDefault(f => f.Id == selectedPackId)
+                    ?.BlackCardsInPack ?? 0);
+
+    private Task AddAllOfficialPacksAsync()
     {
+        _selectedPackIds = _packs.Where(f => f.IsOfficialPack)
+            .Select(f => f.Id)
+            .ToList();
         return Task.CompletedTask;
     }
 
+    private Task AddRandomPackAsync()
+    {
+        _selectedPackIds.Add(_packs.GetRandomElement().Id);
+        return Task.CompletedTask;
+    }
+
+    private bool IsOfficialPack(string packName) => _packs.Any(p => p.Name.Equals(packName, StringComparison.OrdinalIgnoreCase) && p.IsOfficialPack);
+
+    private Task Add5RandomPacksAsync()
+    {
+        var randomPacksToAdd = _packs
+            .ExceptBy(_selectedPackIds, p => p.Id)
+            .Select(p => p.Id)
+            .GetRandomElements(5)
+            .ToList();
+
+        _selectedPackIds.AddRange(randomPacksToAdd);
+
+        return Task.CompletedTask;
+    }
+
+    private Task ClearPacksAsync()
+    {
+        _selectedPackNames.Clear();
+        _selectedPackIds.Clear();
+        return Task.CompletedTask;
+    }
+    #endregion
+
+    #region  Twitch Methods
+    private async Task AddPlayerToListAsync()
+    {
+        var player = await PlayerVerificationService.CheckTwitchForUserAsync(_playerToAdd);
+
+        if (player is null)
+        {
+            await MessageService.Warning($"{_playerToAdd} was not able to be verified", ModalTitle, options =>
+            {
+                options.Size = ModalSize.Small;
+                options.TitleClass = "fw-bold";
+                options.MessageClass = "fw-light";
+                options.ShowMessageIcon = true;
+                options.ShowCloseButton = true;
+            });
+        }
+
+        if (player is not null)
+        {
+            _players.Add(player);
+        }
+
+        _playerToAdd = String.Empty;
+    }
+
+    private static String DeterminePlayerIconName(string broadcasterType)
+    {
+        if (String.IsNullOrWhiteSpace(broadcasterType))
+        {
+            return "fa-brands fa-twitch";
+        }
+
+        return String.Equals(broadcasterType,BroadcasterTypes.Affiliate.ToString(), StringComparison.InvariantCultureIgnoreCase)
+            ? "fa-certificate"
+            : "fa-badge-check";
+    }
+
+
+    private static string DeterminePlayerIconColor(string broadcasterType)
+    {
+        if (String.IsNullOrWhiteSpace(broadcasterType))
+        {
+            return "#fff";
+        }
+
+        return String.Equals(broadcasterType, BroadcasterTypes.Partner.ToString(), StringComparison.InvariantCultureIgnoreCase) 
+            ? $"color: #{TwitchColorCodes.MAIN_COLOR};"
+            : $"color: #{TwitchColorCodes.ACCENT_COLOR};";
+    }
+
+    private Task RemovePlayerAsync(User player)
+    {
+        _players.Remove(player);
+        return Task.CompletedTask;
+    }
+
+    #endregion
     private Task OnCancelClickedAsync()
     => ModalService.Hide();
-
+    #region Disposal Methods
     public void Dispose()
     {
         StringBuilderPoolFactory<GameCodeGenerator>.Remove(nameof(CreateCrowGameComponent));
@@ -54,4 +180,5 @@ public partial class CreateCrowGameComponent : ComponentBase, IDisposable, IAsyn
         GC.SuppressFinalize(this);
         return ValueTask.CompletedTask;
     }
+    #endregion
 }
--- TheOmenDen.CrowsAgainstHumility.Core/Constants/TwitchColorCodes.cs
+++ TheOmenDen.CrowsAgainstHumility.Core/Constants/TwitchColorCodes.cs
@@ -1,15 +1,15 @@
-﻿namespace TheOmenDen.CrowsAgainstHumility.Twitch.Constants;
+﻿namespace TheOmenDen.CrowsAgainstHumility.Core.Constants;
 
-internal static class ColorCodes
+public static class TwitchColorCodes
 {
     /// <summary>
-    /// Twitch's Main colour
+    /// Twitch's Main color
     /// </summary>
     /// <value>RGB:(145,71,255)</value>
     public const string MAIN_COLOR = "9147ff";
 
     /// <summary>
-    /// Twitch's Accent colour
+    /// Twitch's Accent color
     /// </summary>
     /// <value>RGB:(119,44,231)</value>
     public const string ACCENT_COLOR = "772ce7";
